e95adcabd24ccabf81a1e076895d7e0a44bd5799
TuurKlaasenUCLL <tuur.klaasen@student.ucll.be>
2025-12-01 20:06:38 +0100

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ exerciesObserver/src/main/java/com/sc/exerciesObserver/ExerciesObserverApplication.java
@@ -0,0 +1,13 @@
+package com.sc.exerciesObserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciesObserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciesObserverApplication.class, args);
+	}
+
+}
